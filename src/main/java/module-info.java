322154f2c289416c5da4d7650e4b4bca7a335cde
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-09 08:29:13 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,7 @@ module com.tugalsan.api.file.common {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.file;
     requires com.tugalsan.api.list;
-    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.time;
     exports com.tugalsan.api.file.common.client;
     exports com.tugalsan.api.file.common.server;

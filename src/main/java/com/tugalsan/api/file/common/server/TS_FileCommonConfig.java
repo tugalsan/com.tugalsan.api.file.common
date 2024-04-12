4fc837e06415ee5b40d71b081b27b6d2a0469654
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-12 14:40:11 +0300

    Update TS_FileCommonConfig.java

--- src/main/java/com/tugalsan/api/file/common/server/TS_FileCommonConfig.java
+++ src/main/java/com/tugalsan/api/file/common/server/TS_FileCommonConfig.java
@@ -10,6 +10,7 @@ import com.tugalsan.api.list.client.*;
 import com.tugalsan.api.log.server.TS_Log;
 import java.nio.file.*;
 import com.tugalsan.api.time.client.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import java.util.*;
 import com.tugalsan.api.url.client.TGS_Url;
 import com.tugalsan.api.url.client.parser.TGS_UrlParser;
@@ -18,10 +19,10 @@ import java.util.stream.IntStream;
 
 public class TS_FileCommonConfig {
 
-    final private static TS_Log d = TS_Log.of(false, TS_FileCommonConfig.class);
+    private static TS_Log d = TS_Log.of(false, TS_FileCommonConfig.class);
 
-    final public List<TGS_FontFamily<Path>> fontFamilyPaths;
-    final public List<TGS_FontFamily<Font>> fontFamilyFonts;
+    public List<TGS_FontFamily<Path>> fontFamilyPaths;
+    public List<TGS_FontFamily<Font>> fontFamilyFonts;
     public volatile int fontFamilyIdx = 0;
 
     public String fontColor;
@@ -35,7 +36,7 @@ public class TS_FileCommonConfig {
     public String fileNameLabel;
 
     //MACRO BASE
-    final public TGS_Time now;
+    public TGS_Time now;
     public String doFind_gotoLabel = null;
     public String macroLine;
     public String macroLineUpperCase;
@@ -53,40 +54,40 @@ public class TS_FileCommonConfig {
 
     //GLOBALS
     public boolean runReport;
-    final public List<String> macroLines;
-    final public String username;
-    final public String tablename;
-    final public Long selectedId;
-    final public String funcName;
+    public List<String> macroLines;
+    public String username;
+    public String tablename;
+    public Long selectedId;
+    public String funcName;
     public Integer cellHeight;
-    final public String userDotTablename;
+    public String userDotTablename;
 
-    final public TGS_Url url;
-    final public Path dirDat;
-    final public Path dirDatTbl;
-    final public Path dirDatPub;
-    final public Path dirDatUsr;
-    final public Path dirDatUsrTmp;
+    public TGS_Url url;
+    public Path dirDat;
+    public Path dirDatTbl;
+    public Path dirDatPub;
+    public Path dirDatUsr;
+    public Path dirDatUsrTmp;
 
-    final public TGS_FileCommonFavIcon favIconPng;
+    public TGS_FileCommonFavIcon favIconPng;
 
-    final public TGS_CallableType1<TGS_Url, TGS_Url> manipulateInjectCode;
-    final public TGS_CallableType5<List<String>, String, String, Long, String, Boolean> libTableFileList_getFileNames_DataIn;
-    final public TGS_CallableType2<Path, String, String> libTableFileDir_datTblTblnameColname;
-    final public TGS_CallableType2<TGS_Url, String, Boolean> libTableFileGetUtils_urlUsrTmp;
-    final public TGS_CallableType1<String, CharSequence> libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE;
-    final public TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_PUBLIC;
-    final public TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER;
+    public TGS_CallableType1<TGS_Url, TGS_Url> manipulateInjectCode;
+    public TGS_CallableType5<List<String>, String, String, Long, String, Boolean> libTableFileList_getFileNames_DataIn;
+    public TGS_CallableType2<Path, String, String> libTableFileDir_datTblTblnameColname;
+    public TGS_CallableType2<TGS_Url, String, Boolean> libTableFileGetUtils_urlUsrTmp;
+    public TGS_CallableType1<String, CharSequence> libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE;
+    public TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_PUBLIC;
+    public TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER;
 
-    final public TGS_Url bootloaderJs;
-    final public String domainName;
+    public TGS_Url bootloaderJs;
+    public String domainName;
 
     @Override
     public String toString() {
         return TS_FileCommonConfig.class.getSimpleName() + "{" + "runReport=" + runReport + ", username=" + username + ", tablename=" + tablename + ", selectedId=" + selectedId + ", funcName=" + funcName + ", userDotTablename=" + userDotTablename + ", url=" + url + ", dirDat=" + dirDat + '}';
     }
 
-    public static TS_FileCommonConfig of(
+    public static TGS_UnionExcuse<TS_FileCommonConfig> of(
             List<String> macroLines, String username,
             String tablename, Long selectedId,
             String funcName, String fileNameLabel, TGS_Url url,
@@ -122,7 +123,7 @@ public class TS_FileCommonConfig {
         );
     }
 
-    public static TS_FileCommonConfig of(
+    public static TGS_UnionExcuse<TS_FileCommonConfig> of(
             List<String> macroLines, String username,
             String tablename, Long selectedId,
             String funcName, String fileNameLabel, TGS_Url url,
@@ -139,41 +140,7 @@ public class TS_FileCommonConfig {
             TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER,
             TGS_Url bootloaderJs
     ) {
-        return new TS_FileCommonConfig(
-                macroLines, username,
-                tablename, selectedId,
-                funcName, fileNameLabel, url,
-                requestedFileTypes, dirDat,
-                fontFamilyPaths,
-                favIconPng,
-                manipulateInjectCode,
-                dirDatTbl, dirDatPub, dirDatUsr, dirDatUsrTmp,
-                libTableFileList_getFileNames_DataIn,
-                libTableFileDir_datTblTblnameColname,
-                libTableFileGetUtils_urlUsrTmp,
-                libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE,
-                libFileServletUtils_URL_SERVLET_FETCH_PUBLIC,
-                libFileServletUtils_URL_SERVLET_FETCH_USER,
-                bootloaderJs
-        );
-    }
-
-    private TS_FileCommonConfig(
-            List<String> macroLines, String username,
-            String tablename, Long selectedId,
-            String funcName, String fileNameLabel, TGS_Url url,
-            List<String> requestedFileTypes, Path dirDat,
-            List<TGS_FontFamily<Path>> fontFamilyPaths, TGS_FileCommonFavIcon favIconPng,
-            TGS_CallableType1<TGS_Url, TGS_Url> manipulateInjectCode,
-            Path dirDatTbl, Path dirDatPub, Path dirDatUsr, Path dirDatUsrTmp,
-            TGS_CallableType5<List<String>, String, String, Long, String, Boolean> libTableFileList_getFileNames_DataIn,
-            TGS_CallableType2<Path, String, String> libTableFileDir_datTblTblnameColname,
-            TGS_CallableType2<TGS_Url, String, Boolean> libTableFileGetUtils_urlUsrTmp,
-            TGS_CallableType1<String, CharSequence> libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE,
-            TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_PUBLIC,
-            TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER,
-            TGS_Url bootloaderJs
-    ) {
+        var _this = new TS_FileCommonConfig();
         if (d.infoEnable) {
             IntStream.range(0, macroLines.size()).forEachOrdered(i -> {
                 d.ci("macroLines", i, macroLines.get(i));
@@ -204,41 +171,55 @@ public class TS_FileCommonConfig {
             d.ci("libFileServletUtils_URL_SERVLET_FETCH_PUBLIC", libFileServletUtils_URL_SERVLET_FETCH_PUBLIC != null);
             d.ci("libFileServletUtils_URL_SERVLET_FETCH_USER", libFileServletUtils_URL_SERVLET_FETCH_USER != null);
         }
-        this.macroLines = macroLines;
-        this.username = username;
-        this.selectedId = selectedId;
-        this.funcName = funcName;
-        this.fileNameLabel = fileNameLabel;
-        this.url = url;
-        this.requestedFileTypes = requestedFileTypes;
-        this.dirDat = dirDat;
-        this.fontFamilyPaths = fontFamilyPaths;
+        _this.macroLines = macroLines;
+        _this.username = username;
+        _this.selectedId = selectedId;
+        _this.funcName = funcName;
+        _this.fileNameLabel = fileNameLabel;
+        _this.url = url;
+        _this.requestedFileTypes = requestedFileTypes;
+        _this.dirDat = dirDat;
+        _this.fontFamilyPaths = fontFamilyPaths;
 //        this.customDomain = customDomain;
-        this.favIconPng = favIconPng;
-        this.manipulateInjectCode = manipulateInjectCode;
-        this.dirDatTbl = dirDatTbl;
-        this.dirDatPub = dirDatPub;
-        this.dirDatUsr = dirDatUsr;
-        this.dirDatUsrTmp = dirDatUsrTmp;
-        this.libTableFileList_getFileNames_DataIn = libTableFileList_getFileNames_DataIn;
-        this.libTableFileDir_datTblTblnameColname = libTableFileDir_datTblTblnameColname;
-        this.libTableFileGetUtils_urlUsrTmp = libTableFileGetUtils_urlUsrTmp;
-        this.libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE = libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE;
-        this.libFileServletUtils_URL_SERVLET_FETCH_PUBLIC = libFileServletUtils_URL_SERVLET_FETCH_PUBLIC;
-        this.libFileServletUtils_URL_SERVLET_FETCH_USER = libFileServletUtils_URL_SERVLET_FETCH_USER;
-        this.bootloaderJs = bootloaderJs;
+        _this.favIconPng = favIconPng;
+        _this.manipulateInjectCode = manipulateInjectCode;
+        _this.dirDatTbl = dirDatTbl;
+        _this.dirDatPub = dirDatPub;
+        _this.dirDatUsr = dirDatUsr;
+        _this.dirDatUsrTmp = dirDatUsrTmp;
+        _this.libTableFileList_getFileNames_DataIn = libTableFileList_getFileNames_DataIn;
+        _this.libTableFileDir_datTblTblnameColname = libTableFileDir_datTblTblnameColname;
+        _this.libTableFileGetUtils_urlUsrTmp = libTableFileGetUtils_urlUsrTmp;
+        _this.libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE = libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE;
+        _this.libFileServletUtils_URL_SERVLET_FETCH_PUBLIC = libFileServletUtils_URL_SERVLET_FETCH_PUBLIC;
+        _this.libFileServletUtils_URL_SERVLET_FETCH_USER = libFileServletUtils_URL_SERVLET_FETCH_USER;
+        _this.bootloaderJs = bootloaderJs;
 
-        this.fontItalic = false;
-        this.fontItalic = false;
-        this.fontHeight = 12;
-        this.fontColor = TS_FileCommonFontTags.CODE_TOKEN_FONT_COLOR_BLACK();
-        this.runReport = false;
-        this.tablename = tablename;
-        this.now = TGS_Time.of();
-        this.mapVars = TGS_ListUtils.of();
-        this.cellHeight = null;
-        this.userDotTablename = this.username + "." + this.tablename;
-        this.domainName = bootloaderJs == null ? "localhost" : TGS_UrlParser.of(bootloaderJs).host.domain;
-        this.fontFamilyFonts = TS_FontUtils.toFont(fontFamilyPaths, fontHeight);
+        _this.fontItalic = false;
+        _this.fontItalic = false;
+        _this.fontHeight = 12;
+        _this.fontColor = TS_FileCommonFontTags.CODE_TOKEN_FONT_COLOR_BLACK();
+        _this.runReport = false;
+        _this.tablename = tablename;
+        _this.now = TGS_Time.of();
+        _this.mapVars = TGS_ListUtils.of();
+        _this.cellHeight = null;
+        _this.userDotTablename = _this.username + "." + _this.tablename;
+        if (bootloaderJs == null) {
+            _this.domainName = "localhost";
+        } else {
+            var u = TGS_UrlParser.of(bootloaderJs);
+            if (u.isExcuse()) {
+                return u.toExcuse();
+            }
+            _this.domainName = u.value().host.domain;
+        }
+        _this.fontFamilyFonts = TS_FontUtils.toFont(fontFamilyPaths, _this.fontHeight);
+        return TGS_UnionExcuse.of(_this);
     }
+
+    private TS_FileCommonConfig() {
+
+    }
+
 }
